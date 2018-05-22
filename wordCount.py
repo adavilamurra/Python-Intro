dd7f9eb47d91a6d36a249755e9fbc2321d19d2ad
Alejandro Davila <alejandro.davila@ttu.edu>
2018-05-22 18:22:22 -0500

    Added Python project

---
+++ wordCount.py
@@ -0,0 +1,30 @@
+import sys  #command line
+import re   #regular expression tools
+import os   #checking if file exists
+import subprocess   #executing program
+
+#get files
+inputFname = sys.argv[1] 
+outputFname = sys.argv[2]
+
+#create dictionary
+dictionary = dict()
+
+#read from input file
+with open(inputFname, "r") as textInputFile:
+    #use regex to get words and ignore punctuation
+    words = (re.split(r"\s*[.]*[,]*[;]*[:]*[\"]*[']*[-]*", textInputFile.read()))
+    for word in words:
+	if not word == "":
+                word = word.lower() #change words to lowercase
+		if word in dictionary:
+			dictionary[word] += 1 #if word is repeated, increase count
+		else:
+			dictionary[word] = 1 #if first occurrence, set count to 1
+    textInputFile.close() #close file
+
+#write on output file
+with open(outputFname, "w+") as textOutputFile:
+    for w in sorted(dictionary):
+        textOutputFile.write("%s %s" % (w, dictionary[w]) + "\n") #write in file with format: '#OfOcurrences word'
+    textOutputFile.close()#close file
